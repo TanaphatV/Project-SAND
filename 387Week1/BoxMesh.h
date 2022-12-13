f4aa880c6cbc3f350af6ed71545bd796accca2de
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2022-12-14 01:54:29 +0700

    accidentally deleted boxMesh.h

---
+++ 387Week1/BoxMesh.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Mesh.h"
+#include <GL\glew.h>
+class BoxMesh :
+  public Mesh
+{
+private:
+	static BoxMesh* instance;
+	GLuint vboId2 = 0;
+	BoxMesh();
+    glm::vec3 getNormal(glm::vec3 v1, glm::vec3 v2, glm::vec3 v3);
+public:
+	virtual void draw(GLuint colorId, glm::vec4 color, GLuint matrixId, glm::mat4 transform);
+	void drawInstance(GLRenderer* renderer, glm::vec4 color, vector<glm::mat4>& m, int amount);
+	virtual void loadData(GLRenderer* renderer);
+	static BoxMesh* getInstance();
+	GLuint getVao() {
+		return vaoId;
+	}
+};
+
